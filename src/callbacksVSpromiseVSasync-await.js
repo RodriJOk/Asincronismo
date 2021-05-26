5c37d565541092f955d8db33efa79d71298ffc89
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-26 16:16:33 -0300

    Add file callback vs promise vs async await

---
+++ src/callbacksVSpromiseVSasync-await.js
@@ -0,0 +1,19 @@
+// ------------ Callbacks, Async Await y Promesas ------------
+// ------------ ¿Por cual me decido? Esa es la cuestion ------------
+
+//Lo cierto es que podemos elegir alguna de ellas segun lo necesitemos. 
+//Algo que nos podra ser de mucha ayuda, sera repasar las ventajas y desventajas de cada uno. 
+
+// ------------ Callbacks ------------
+
+//Ventajas 
+//Desventajas
+
+// ------------ Promise ------------
+
+//Ventajas
+//Desventajas
+
+// ------------ Async-Await ------------
+//Ventajas
+//Desventajas
